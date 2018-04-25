7cc394ce903426cc390ecb931c24174fdd4a3fc5
Mary Alice Orr <m.a.orr17@gmail.com>
2018-04-25 18:12:29 -0500

    made fixes to data.js and added getMessagesByUser

--- javascripts/data.js
+++ javascripts/data.js
@@ -8,7 +8,18 @@ const setMessages = (messagesArray) => {
   messageData = messagesArray;
 };
 
+const getMessagesByUser = (userId) => {
+  const selectedMessages = [];
+  selectedMessages.forEach((messageData) => {
+    if (messageData.userId === userId) {
+      selectedMessages.push(messageData);
+    }
+  });
+  return selectedMessages;
+};
+
 module.exports = {
   getMessages,
   setMessages,
+  getMessagesByUser,
 };
