489fedeeacccd47a68676aaf3acef0fec33d83ff
Anna Dolan <anna.dolan115@gmail.com>
2018-04-25 18:18:09 -0500

    wip

---
+++ javascripts/dom.js
@@ -0,0 +1,16 @@
+// const messagesDiv = document.getElementById('message-container');
+
+// const preloadMessages = (messages) => {
+//   let domString = "";
+//   messages.forEach((message) => {
+//     domString += `<div id = "message-card">
+//                     ${message.message}
+//                   </div>`;
+//   )};
+// };
+
+// const printToDom = () => {
+//   messagesDiv.innerHTML = domString(messages);
+// };
+
+// module.exports = printToDom;
